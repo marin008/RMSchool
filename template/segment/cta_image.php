3f788f089795becddca17b7f0d06fd60f14af301
Marin <marin.galic@creative-square.agency>
2020-09-17 16:54:31 +0200

    e

--- template/segment/cta_image.php
+++ template/segment/cta_image.php
@@ -1,14 +1,22 @@
-<section class="cta-image container">
+<section class="cta-image container-fluid px-0">
     <div class="row">
-        <h2 class="col-12">Active learning with <span>hands-on teaching material</span></h2>
-        <img src="assets/img/jpg/lab.jpg" alt="." class="img-fluid pozadina">
+        <div class="container">
+            <h2 class="col-12">Active learning with <span>hands-on teaching material</span></h2>
+        </div>
+    </div>
 
-        
-        <div class="card col-12 col-md-8 col-lg-6">
-            <div class="card-body">
-                <h3 class="card-title">Teaching Resources</h3>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, laborum sequi sed recusandae quidem dolorem provident omnis maxime sapiente aliquam ex quisquam aspernatur id dolor libero, accusamus reiciendis commodi beatae?</p>
-                <a href="#!" class="btn btn-outline-white">Go somewhere</a>
+    <div class="row row2 mt-5 px-0">
+        <div class="col col-lg-9 mx-auto">
+            <div class="img-container pozadina">
+                <img src="assets/img/jpg/lab.jpg" alt="." class="">
+            </div>
+            
+            <div class="card col col-md-8 col-lg-8 col-xl-6">
+                <div class="card-body">
+                    <h3 class="card-title">Teaching Resources</h3>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, laborum sequi sed recusandae quidem dolorem provident omnis maxime sapiente aliquam ex quisquam aspernatur id dolor libero, accusamus reiciendis commodi beatae?</p>
+                    <a href="#!" class="btn btn-outline-white">Go somewhere</a>
+                </div>
             </div>
         </div>
     </div>
--- template/segment/hero_rotator.php
+++ template/segment/hero_rotator.php
@@ -6,30 +6,14 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-        <img src="assets/img/jpg/lab.jpg" alt="." class="img-fluid pozadina"> 
+        <img src="assets/img/jpg/lab.jpg" alt="." class="pozadina"> 
         <div class="item-text container py-5">
           <h2 class="col-3 mt-5">RawMatTERS Ambassadors at Schools</h2>
           <h4 class="col-3 mb-5">Insolve youngsters in science technology & sustainability</h4>
           <button type="button" class="btn btn-success mb-5">Learn More</button>
         </div>
     </div>
-    <div class="carousel-item">
-        <img src="assets/img/jpg/lab.jpg" alt="." class="img-fluid pozadina"> 
-        <div class="item-text container py-5">
-          <h2 class="col-3 mt-5">RawMatTERS Ambassadors at Schools</h2>
-          <h4 class="col-3 mb-5">Insolve youngsters in science technology & sustainability</h4>
-          <button type="button" class="btn btn-success mb-5">Learn More</button>
-        </div>
-    </div>
-    <div class="carousel-item">
-        <img src="assets/img/jpg/lab.jpg" alt="." class="img-fluid pozadina"> 
-        <div class="item-text container py-5">
-          <h2 class="col-3 mt-5">RawMatTERS Ambassadors at Schools</h2>
-          <h4 class="col-3 mb-5">Insolve youngsters in science technology & sustainability</h4>
-          <button type="button" class="btn btn-success mb-5">Learn More</button>
-        </div>
-    </div>
-  </div>
+      
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon fa fa-angle-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
